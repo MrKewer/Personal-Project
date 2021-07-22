34f525d71ad70abe5a3217fce46128dfbedc966b
MrKewer <danie_grobbelaar@yahoo.com>
2021-07-22 15:09:14 +0200

    Update Player + new Scripts
    Initiate Character Selected
    Added new empty scripts: Singleton, UIManager, GameManager

--- Programming Theory Project/Assets/Scripts/PlayerController.cs
+++ Programming Theory Project/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private GameObject characterSelected;
+    [SerializeField] private GameObject CharacterListPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 characterTransform = new Vector3(0, 0, 0);
+        Quaternion characterRotation = Quaternion.Euler(0, 90, 0);
+        characterSelected = CharacterListPrefab.GetComponent<CharacterList>().characterList[1];
+        characterSelected = Instantiate(characterSelected,characterTransform, characterRotation);
     }
 
     // Update is called once per frame
