9d3d35eaa3a62c57cda97fff2379dd51d1ca0ef8
MrKewer <danie_grobbelaar@yahoo.com>
2021-07-31 16:56:17 +0200

    Update on EndGameUI

--- Programming Theory Project/Assets/Scripts/UI/EndGameMenu.cs
+++ Programming Theory Project/Assets/Scripts/UI/EndGameMenu.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndGameMenu : MonoBehaviour
 {
+    [SerializeField] private Button exitButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        exitButton.onClick.AddListener(ExitToMain);
     }
 
-    // Update is called once per frame
-    void Update()
+    void ExitToMain()
     {
-        
+        GameManager.Instance.ExitToMain();
     }
 }
